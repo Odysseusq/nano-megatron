d95448daa9638b4e3f463ae05d6d43614ea2337a
Odysseusq <57209123+Odysseusq@users.noreply.github.com>
2026-02-06 04:50:45 -0500

    fix grad norm calc

--- nanomegatron/optim/grad_accumulator.py
+++ nanomegatron/optim/grad_accumulator.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 import torch
+import torch.distributed as dist
 from torch import nn
 
 
@@ -52,10 +53,10 @@ class FP32GradientAccumulator:
     def zero_grad(self):
         self._contiguous_grad_buffer.zero_()
 
-    def clip_grad(self, max_norm: float, norm_type: float = 2.0) -> torch.Tensor:
-        total_norm = torch.linalg.vector_norm(
-            self._contiguous_grad_buffer, ord=norm_type, dtype=torch.float
-        )
+    def clip_grad(self, max_norm: float) -> torch.Tensor:
+        local_norm_square = self._contiguous_grad_buffer.dot(self._contiguous_grad_buffer)
+        dist.all_reduce(local_norm_square, op=dist.ReduceOp.SUM)
+        total_norm = local_norm_square.sqrt()
         clip_coef = torch.clamp(max_norm / (total_norm + 1e-6), max=1.0)
         self._contiguous_grad_buffer.mul_(clip_coef)
         return total_norm
